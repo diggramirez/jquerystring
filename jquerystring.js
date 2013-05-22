cfbdad2b96736602173839623d42b146455b0d76
diggramirez <elvinrodas@outlook.com>
2013-05-21 22:01:16 -0400

    function shorted

--- jquerystring.js
+++ jquerystring.js
@@ -2,14 +2,14 @@
 * JQueryString - v1.0 - 2013-05-21
 * https://github.com/diggramirez/jquerystring
 * Copyright (c) 2013 Elvin Rodas
- * Licensed MIT (https://github.com/diggramirez/jquerystring/LICENSE.md)
+* Licensed MIT (https://github.com/diggramirez/jquerystring/LICENSE.md)
 */
 
 /*
 @key required
 @url optional, allow to pass string as url and get the specified key
 */
-function jquerystring_value( key, url )
+function jquerystring( key, url )
 {
   if ( url == null )
   {
